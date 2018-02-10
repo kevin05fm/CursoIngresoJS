e0f34c1f56bc8f2040ac357ee10d7647dd7ed908
kevin05fm <36281658+kevin05fm@users.noreply.github.com>
2018-02-09 22:14:17 -0300

    IF ejercicios 1-2-3-4-5

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,15 @@
+function Mostrar()
+
+
+{
+//tomo la edad  
+var edad
+
+edad=document.getElementById('edad').value;
+edad=15;
+
+if(edad==15)
+	{alert("niña bonita"); }
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+//tomo la edad  
+var edad
+
+edad=document.getElementById('edad').value;
+edad=parseInt(edad);
+
+if(edad>=18)
+
+{alert("mayor")}
+else
+{alert("menor")}
+
+}//FIN DE LA FUNCIÓN

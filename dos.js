589149d09900545e52f40d2fa1f07c39e00d86a9
kevin05fm <36281658+kevin05fm@users.noreply.github.com>
2018-02-13 23:27:52 -0300

    parcial ejercicio 1-2

---
+++ dos.js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+  var importe;
+
+   
+  document.getElementById('importeFinal').value=importe;
+
+  importe=parseInt(importe);
+  importe=(importe*21)/100;
+  
+
+ prompt("el importe es ");
+  
+
+
+}
---
+++ uno.js
@@ -0,0 +1,12 @@
+
+function Mostrar()
+{
+    var base;
+
+    base=document.getElementById('laBase').value;
+
+    base=parseInt(base);
+
+    alert(base*4);
+    
+}

c2f73660e0366e57bf833d226ebc169e9e68c458
kevin05fm <36281658+kevin05fm@users.noreply.github.com>
2018-02-16 22:14:00 -0300

    ejercicios switch

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,36 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+
+switch(mesDelAño)
+	{
+		case"Enero":
+		case"Febrero":
+		case"Marzo":
+		case"Abril":
+		case"Mayo":
+		case"Junio":
+			alert("falta para el invierno");
+
+			break
+		case"Julio":
+		case"Agosto":
+			alert("Abrigate que hace frio");
+
+			break
+
+		case"Septiembre":
+		case"Octubre":
+		case"Noviembre":
+		case"Diciembre":
+			alert("Ya pasamos el frio,ahora el calor");
+
+			break		
+
+
+	}
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,26 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+	
+	switch(mesDelAño)
+	{
+		case"Febrero":
+			alert("Este mes no tieme mas de 29 dias");
+
+			break
+
+		default:
+			alert("Este mes tiene 30 o mas dias");		
+
+
+
+
+	}
+
+
+
+
+}//FIN DE LA FUNCIÓN
